f3978824217efbbf1125cf43752e434a46344de6
Code-npm <myukti20@gmail.com>
2025-11-21 17:08:57 +0530

    day48

---
+++ day48/script.js
@@ -0,0 +1,18 @@
+arr=[
+    {
+        Name:"Yukti Maheshwari",
+        Work:"Robotics Engineer",
+        Description:"To learn and implement pcb which is small and can run heavy AI model"
+    },
+    {
+        Name:"Sijal Rajput",
+        Work:"Math Professor",
+        Description:"Enthusiasts Math Professor "
+    },
+    {
+        Name:"Ankit Sharma",
+        Work:"CS Engineer",
+        Description:"AI - Artificial Inteligence ,"
+    }
+
+]
