74986b58e3e332af6509218a1d672dd63e40561e
Code-npm <myukti20@gmail.com>
2025-11-21 09:41:27 +0530

    day47

--- day47/script.js
+++ day47/script.js
@@ -1,8 +1,18 @@
-var img=document.querySelector('.img')
-var card=document.querySelector('.card')
-var love=document.querySelector('.love')
-img.addEventListener('click' , function(){
-     love.style.opacity='1'
-     love.style.transform="translate(-50%,-50%) scale(1) rotate(0deg);"
-     love.style.backgroundColor='pink'
+var heart=document.querySelector('i')
+var img=document.querySelector('img')
+
+img.addEventListener('click', function(){
+     heart.style.opacity=1
+     heart.style.transform="translate(-50%,-50%) scale(1) rotate(0deg)"
+
+     setTimeout(function(){
+       heart.style.transform="translate(-50%,-200%) scale(1) rotate(60deg)"
+
+     },800)
+     setTimeout(function(){
+       heart.style.opacity=0
+     },900)
+     setTimeout(function(){
+       heart.style.transform="translate(-50%,-50%) scale(0) rotate(-60deg)"
+     },1100)
 })
