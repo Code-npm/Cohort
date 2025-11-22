d186ca3d5104469adc0a0f57f698a64d0914cad3
Code-npm <myukti20@gmail.com>
2025-11-22 15:41:08 +0530

    changes2

--- day49/script.js
+++ day49/script.js
@@ -1,31 +1,31 @@
 document.body.addEventListener("keydown" , function(dets){
     console.log(dets)
    if(dets.key=="a"){
-    document.getElementById('myAudio1').play()
+        document.getElementById('myAudio1').play()
    }
    else if(dets.key=="d"){
        document.getElementById('myAudio2').play()
    }
    else if(dets.key=='e'){
-    document.getElementById('myAudio3').play()
+       document.getElementById('myAudio3').play()
    }
    else if(dets.key=='b'){
-    document.getElementById('myAudio4').play()
+       document.getElementById('myAudio4').play()
    }
    else if(dets.key=='c'){
-    document.getElementById('myAudio5').play()
+       document.getElementById('myAudio5').play()
    }
    else if(dets.key=='f'){
-    document.getElementById('myAudio6').play()
+        document.getElementById('myAudio6').play()
    }
    else if(dets.key=='g'){
-    document.getElementById('myAudio7').play()
+        document.getElementById('myAudio7').play()
    }
    else if(dets.key=='h'){
-    document.getElementById('myAudio8').play()
+        document.getElementById('myAudio8').play()
    }
    else if(dets.key=='i'){
-    document.getElementById('myAudio9').play()
+        document.getElementById('myAudio9').play()
    }
    
 })
